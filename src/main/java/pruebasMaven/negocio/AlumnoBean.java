5d3fcee41e1cedcbba7a9e1ae2c7c8a5e7013336
diegoWin <Diego@HP-Diego>
2019-10-22 22:13:17 +0200

    3º Commit ya funciona CreateAlumno en H2

--- src/main/java/pruebasMaven/negocio/AlumnoBean.java
+++ src/main/java/pruebasMaven/negocio/AlumnoBean.java
@@ -1,11 +1,13 @@
 package pruebasMaven.negocio;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Entity
 @Table(name="alumno")
 public class AlumnoBean {
 	@Id
